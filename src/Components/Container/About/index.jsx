34aca8958903c786a6cf30214a936dd349bd6476
Sudhir Yadav <72488699+sudhir-code@users.noreply.github.com>
2024-03-12 15:49:23 +0530

    Update index.jsx

--- src/Components/Container/About/index.jsx
+++ src/Components/Container/About/index.jsx
@@ -80,6 +80,13 @@ function About() {
 
   <Col lg={3} md ={4} className=" mt-4 mt-md-0">
     <div className="icon-box">
+    <RiCodeSSlashFill  style={{color: "#28a745"}} className='ri'/>
+      <h3>Backend Development</h3>
+    </div>
+  </Col>
+  
+  <Col lg={3} md ={4} className=" mt-4 mt-md-0">
+    <div className="icon-box">
       <RiFileList3Line style={{color: "#47aeff"}} className='ri'/>
       <h3>Algorithms</h3>
     </div>
